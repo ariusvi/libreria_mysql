0d403e249627dedb48657ce22eddfdfa2240fa41
Ana Rius <a.riusvi11@gmail.com>
2024-02-21 19:13:45 +0100

    creacion archivos base

---
+++ libreria.sql
@@ -0,0 +1,5 @@
+/* En una librería Online, solo alquilamos libros. Cada libro pertenece a un autor. Los
+clientes exploran nuestro catálogo de libros, seleccionan un libro y lo añaden a la
+cesta de la compra. Al finalizar la compra su cesta se convierte en un pedido efectivo
+que se envía al cliente. Los clientes pueden añadir libros a sus favoritos */
+
