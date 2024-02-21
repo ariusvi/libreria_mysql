d17a12c9a64fe8773d871a94c78c7af943785b30
Ana Rius <a.riusvi11@gmail.com>
2024-02-21 20:24:19 +0100

    creacion tablas authors y books, y relacion entre ellas

--- libreria.sql
+++ libreria.sql
@@ -3,3 +3,15 @@ clientes exploran nuestro catálogo de libros, seleccionan un libro y lo añaden
 cesta de la compra. Al finalizar la compra su cesta se convierte en un pedido efectivo
 que se envía al cliente. Los clientes pueden añadir libros a sus favoritos */
 
+CREATE TABLE authors (
+	id	INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE books (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	authors_id INT, 
+    title VARCHAR(255),
+    genere VARCHAR(50),
+    FOREIGN KEY (authors_id) REFERENCES authors(id)
+);
